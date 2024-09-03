1dccc9f29e7a7b95455a3649f683736769187811
Brossier Thomas <o22301110@or-iut-i102-f26.iut45.univ-orleans.fr>
2024-09-03 15:29:23 +0200

    Rajout de templates

--- tuto/views.py
+++ tuto/views.py
@@ -1,5 +1,14 @@
 from .app import app
+from flask import render_template
+
+# @app.route("/")
+# def home():
+#    return "<h1> Hello World </h1>"
+
 
 @app.route("/")
 def home():
-    return "<h1>Hello World</h1>"
+    return render_template(
+        "home.html",
+        title="Hello World !",
+        names=["Pierre", "Paul", "Corinne"])
