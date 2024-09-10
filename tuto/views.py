8dc9532c0c6cc463f109ae6b5237e0858cec9fa0
Thomas Brossier <thomas.brossier@etu.univ-orleans.fr>
2024-09-10 15:33:57 +0200

    MAJ

--- tuto/views.py
+++ tuto/views.py
@@ -1,14 +1,23 @@
 from .app import app
 from flask import render_template
+from .models import get_sample
 
 # @app.route("/")
 # def home():
 #    return "<h1> Hello World </h1>"
 
 
+# @app.route("/")
+# def home():
+#     return render_template(
+#         "home.html",
+#         title="Hello World !",
+#         names=["Pierre", "Paul", "Corinne"])
+
 @app.route("/")
 def home():
     return render_template(
         "home.html",
-        title="Hello World !",
-        names=["Pierre", "Paul", "Corinne"])
+        title="My Books !",
+        books=get_sample()
+    )
