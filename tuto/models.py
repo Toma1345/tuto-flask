b38ab0da7ad8429679d573be28551f83000d7100
Thomas Brossier <thomas.brossier@etu.univ-orleans.fr>
2024-09-17 15:31:11 +0200

    Requetes shell

--- tuto/models.py
+++ tuto/models.py
@@ -4,6 +4,9 @@ class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
 
+    def __repr__(self):
+        return "<Author (%d) %s" % (self.id, self.name)
+
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Float)
@@ -14,5 +17,9 @@ class Book(db.Model):
     author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
     author = db.relationship("Author", backref=db.backref("books", lazy="dynamic"))
 
+    def __repr__(self):
+        return "<Book (%d) %s" % (self.id, self.title)
+
 def get_sample():
     return Book.query.limit(10).all()
+
